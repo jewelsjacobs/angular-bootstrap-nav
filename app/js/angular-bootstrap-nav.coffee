5b8e168859fe552593566e1f34168f546403ad7b
Julia Jacobs <julia.jacobs@rackspace.com>
2014-05-13 12:11:27 -0500

    (feat): search input & dropdown action button

--- app/js/angular-bootstrap-nav.coffee
+++ app/js/angular-bootstrap-nav.coffee
@@ -1,38 +1,35 @@
-angular.module("bootstrap.navbar", []).directive "bootstrapNav", [->
+angular.module("bootstrap.navbar", ['ngRoute', 'ngSanitize'])
+.controller('NavCtr', [
+  "$scope"
+  "$location"
+  "$route"
+  NavCtr = ($scope, $location, $route) ->
+
+    $scope.navClass = (page) ->
+      currentRoute = $location.path().substring(1).split("/")[0]
+      (if page is currentRoute then "active" else "")
+
+    $scope.routes = []
+    angular.forEach $route.routes, (value, key) ->
+      if value.navitem
+        routeitem = {}
+        routeitem.path = value.originalPath
+        routeitem.name = value.name
+        routeitem.templateUrl = value.templateUrl
+        routeitem.controller = value.controller
+        $scope.routes.push routeitem
+
+    $scope.isCollapsed = true
+])
+.directive("bootstrapNav", ->
   "use strict"
   restrict: "AE"
   replace: true
   transclude: false
   scope:
-    title: "@"
-    logo: '=?'
-    search: '=?'
-
-  controller: [
-    "$scope"
-    "$location"
-    "$route"
-    ($scope, $location, $route) ->
-      $scope.navClass = (page) ->
-        currentRoute = $location.path().substring(1).split("/")[0]
-        (if page is currentRoute then "active" else "")
-
-      $scope.routes = []
-      angular.forEach $route.routes, (value, key) ->
-        if value.navitem
-          routeitem = {}
-          routeitem.path = value.originalPath
-          routeitem.name = value.name
-          routeitem.templateUrl = value.templateUrl
-          routeitem.controller = value.controller
-          $scope.routes.push routeitem
-        return
-
-      $scope.isCollapsed = true
-
-      console.log $scope
-
-      return $scope
-  ]
+    'title': "@"
+    'logo': '=?'
+    'search': '=?'
+  controller: 'NavCtr'
   templateUrl: 'angular-bootstrap-nav.html'
-]
+)
--- dist/angular-bootstrap-nav.js
+++ dist/angular-bootstrap-nav.js
@@ -3,95 +3,47 @@
  * 
  */
 (function() {
-  angular.module("bootstrap.navbar", []).directive("bootstrapNav", [
-    function() {
-      "use strict";
-      return {
-        restrict: "AE",
-        replace: true,
-        transclude: false,
-        scope: {
-          title: "@",
-          logo: '=?',
-          search: '=?'
-        },
-        controller: [
-          "$scope", "$location", "$route", function($scope, $location, $route) {
-            $scope.navClass = function(page) {
-              var currentRoute;
-              currentRoute = $location.path().substring(1).split("/")[0];
-              if (page === currentRoute) {
-                return "active";
-              } else {
-                return "";
-              }
-            };
-            $scope.routes = [];
-            angular.forEach($route.routes, function(value, key) {
-              var routeitem;
-              if (value.navitem) {
-                routeitem = {};
-                routeitem.path = value.originalPath;
-                routeitem.name = value.name;
-                routeitem.templateUrl = value.templateUrl;
-                routeitem.controller = value.controller;
-                $scope.routes.push(routeitem);
-              }
-            });
-            $scope.isCollapsed = true;
-            console.log($scope);
-            return $scope;
-          }
-        ],
-        templateUrl: 'angular-bootstrap-nav.html'
+  var NavCtr;
+
+  angular.module("bootstrap.navbar", ['ngRoute', 'ngSanitize']).controller('NavCtr', [
+    "$scope", "$location", "$route", NavCtr = function($scope, $location, $route) {
+      $scope.navClass = function(page) {
+        var currentRoute;
+        currentRoute = $location.path().substring(1).split("/")[0];
+        if (page === currentRoute) {
+          return "active";
+        } else {
+          return "";
+        }
       };
+      $scope.routes = [];
+      angular.forEach($route.routes, function(value, key) {
+        var routeitem;
+        if (value.navitem) {
+          routeitem = {};
+          routeitem.path = value.originalPath;
+          routeitem.name = value.name;
+          routeitem.templateUrl = value.templateUrl;
+          routeitem.controller = value.controller;
+          return $scope.routes.push(routeitem);
+        }
+      });
+      return $scope.isCollapsed = true;
     }
-  ]);
+  ]).directive("bootstrapNav", function() {
+    "use strict";
+    return {
+      restrict: "AE",
+      replace: true,
+      transclude: false,
+      scope: {
+        'title': "@",
+        'logo': '=?',
+        'search': '=?'
+      },
+      controller: 'NavCtr',
+      templateUrl: 'angular-bootstrap-nav.html'
+    };
+  });
 
 }).call(this);
-
-angular.module("bootstrap.navbar").run(["$templateCache", function($templateCache) {
-
-  $templateCache.put("angular-bootstrap-nav.html",
-    "<nav class=\"navbar navbar-inverse navbar-fixed-top\" role=\"navigation\">\n" +
-    "  <div class=\"navbar-header\">\n" +
-    "    <button type=\"button\" class=\"navbar-toggle\" ng-init=\"navCollapsed = true\" ng-click=\"navCollapsed = !navCollapsed\">\n" +
-    "      <span class=\"sr-only\">Toggle navigation</span>\n" +
-    "      <span class=\"icon-bar\"></span>\n" +
-    "      <span class=\"icon-bar\"></span>\n" +
-    "      <span class=\"icon-bar\"></span>\n" +
-    "    </button>\n" +
-    "    <div ng-if=\"logo\" class=\"pull-left\" ng-bind-html=\"logo\"></div>\n" +
-    "    <a id=\"Ludicbrand\" class=\"navbar-brand\" href=\"#/home\">\n" +
-    "      <span class=\"thin\" ng-bind=\"title\"></span>\n" +
-    "    </a>\n" +
-    "  </div>\n" +
-    "  <div class=\"collapse navbar-collapse\" ng-class=\"!navCollapsed && 'in'\">\n" +
-    "    <ul class=\"nav navbar-nav\">\n" +
-    "      <li ng-repeat=\"route in routes\" data-ng-class=\"navClass('{{route.name}}')\">\n" +
-    "        <a ng-href=\"#{{route.path}}\" ng-bind=\"route.name\"></a>\n" +
-    "      </li>\n" +
-    "    </ul>\n" +
-    "    <form ng-if=\"search\" class=\"navbar-form navbar-left\" role=\"search\">\n" +
-    "      <div ng-if=\"search\" class=\"form-group navbar-left\">\n" +
-    "        <input type=\"text\" ng-model=\"selected\" placeholder=\"{{ search.placeholder }}\" typeahead=\"{{ search.typeahead }}\" typeahead-template-url=\"{{ search.template }}\" class=\"form-control\">\n" +
-    "        <!-- Single button -->\n" +
-    "        <div class=\"btn-group\" dropdown>\n" +
-    "          <button type=\"button\" class=\"btn btn-primary dropdown-toggle\">\n" +
-    "            {{ search.placeholder }} <span class=\"caret\"></span>\n" +
-    "          </button>\n" +
-    "          <ul class=\"dropdown-menu\" role=\"menu\">\n" +
-    "            <li ng-repeat=\"choice in search.button.choices\">\n" +
-    "              <a ng-click=\"current = choice\">\n" +
-    "                {{ choice }}\n" +
-    "              </a>\n" +
-    "            </li>\n" +
-    "          </ul>\n" +
-    "        </div>\n" +
-    "      </div>\n" +
-    "    </form>\n" +
-    "   </div>\n" +
-    "</nav>\n"
-  );
-
-}]);
